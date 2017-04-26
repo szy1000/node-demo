ac4ed88e8c347727289453145399736179faceec
francis <francisgtw@gmail.com>
2017-04-26 15:50:04 +0800

    get one bug

--- blog/app.js
+++ blog/app.js
@@ -9,12 +9,18 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
+var MongoStore = require('connect-mongo')(express);
+var settings = require('./settings');
+var flash = require('connect-flash');
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(flash());
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 
@@ -24,6 +30,18 @@ app.use(express.bodyParser());
 // app.use(express.urlencoded());
 
 app.use(express.methodOverride());
+
+app.use(express.cookieParser());
+app.use(express.session({
+	secret: settings.cookieSecret,
+	key: settings.db,//cookie name
+	cookie: {maxAge: 1000 * 60 * 60 * 24 * 30}, //30 days
+	store: new MongoStore({
+		db: settings.db,
+		url:'mongodb://localhost:27017'
+	})
+}))
+
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
